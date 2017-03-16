8ee0e84ff07c101957b1f7b4f527f08f5d201d20
talewoods <talewoods@sina.com>
2017-03-16 23:41:53 +0800

    函数指针 野指针2017年 03月 16日 星期四 23:41:52 CST

--- C/tmp.c
+++ C/tmp.c
@@ -1,10 +1,27 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+void fun(){
+   
+   printf("ok fun\n");
+}
+
+void fun1(){
+   printf("ok fun1\n");
+}
 
 int main(){
-    int a[7];
-    printf("%d\n",sizeof(a));
-    printf("%d\n",sizeof(a[8]));
-    printf("%d\n",sizeof(&a[0]));
-    printf("%d\n",sizeof(&a));
+
+    int* b=NULL;
+    free(b);
+    free(b);
+    int *a=(int*)malloc(10*sizeof(int));
+    *(a+1)=5;
+    printf("%d\n",*b);
+    free(a);
+    free(a);
+    printf("%d\n",*(a+1));
 }
+
+
 
